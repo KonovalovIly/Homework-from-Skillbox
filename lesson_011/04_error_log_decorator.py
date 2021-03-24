86093bd2baadf433bca3b50040a1cdd1508b115f
xaker0987 <Flatrin0987>
2021-03-24 14:46:37 +0400

    Error decorator

---
+++ lesson_011/04_error_log_decorator.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+
+# Написать декоратор, который будет логировать (записывать в лог файл)
+# ошибки из декорируемой функции и выбрасывать их дальше.
+#
+# Имя файла лога - function_errors.log
+# Формат лога: <имя функции> <тип ошибки> <текст ошибки>
+# Лог файл открывать каждый раз при ошибке в режиме 'a'
+
+
+def log_errors(func):
+    def check_error(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as exc:
+            file_error = open('function_errors.txt', 'a')
+            file_error.write(f'{func, exc}\n')
+    return check_error
+
+
+# Проверить работу на следующих функциях
+@log_errors
+def perky(param):
+    return param / 0
+
+
+@log_errors
+def check_line(lin):
+    name, email, age = lin.split(' ')
+    if not name.isalpha():
+        raise ValueError("it's not a name")
+    if '@' not in email or '.' not in email:
+        raise ValueError("it's not a email")
+    if not 10 <= int(age) <= 99:
+        raise ValueError('Age not in 10..99 range')
+
+
+lines = [
+    'Ярослав bxh@ya.ru 600',
+    'Земфира tslzp@mail.ru 52',
+    'Тролль nsocnzas.mail.ru 82',
+    'Джигурда wqxq@gmail.com 29',
+    'Земфира 86',
+    'Равшан wmsuuzsxi@mail.ru 35',
+]
+
+for line in lines:
+    check_line(line)
+
+perky(param=42)
